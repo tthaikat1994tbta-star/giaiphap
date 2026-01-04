5bc37f381b3eb154c7b3998694eaef9755a26635
tthaikat1994tbta-star <tthaikat1994tbta@gmail.com>
2026-01-04 12:25:04 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+# FILE NÀY CHỈ ĐỂ CHỈNH MENU VÀ THÔNG TIN QUÁN
+TEN_QUAN = "THÁI Ô CÊ"
+NGAN_HANG = "ICB" 
+STK = "107885081042"
+TEN_CHU_TK = "NGUYEN THE THAI"
+
+# Anh muốn thêm món thì cứ viết thêm vào đây nhé
+menu = {
+    "Bia Tiger": 20000, 
+    "Bia Heineken": 25000, 
+    "Lac rang": 15000, 
+    "Nam nuong": 120000, 
+    "Lau Thai": 250000, 
+    "Khan lanh": 3000
+}
